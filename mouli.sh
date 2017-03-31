af4de69264c4c7d2beaecfe8913cac9d17c09e25
keuhdall <lmarques@student.42.fr>
2017-03-30 22:10:43 -0700

    now works with reads instead of arguments

--- mouli.sh
+++ mouli.sh
@@ -2,7 +2,10 @@
 
 # YOU MUST GIVE AS A FIRST ARGUMENT THE REPO URL STARTING WITH intra/20xx/etc
 # AS A SECOND ARGUMENT THE PATH TO THE GIT REPO CLONED
-
+echo "Enter repo URL :"
+read -r URL
+echo "Enter git repository path :"
+read -r GITPATH
 echo "= Host-specific information ====================================================
 $> hostname; uname -msr"
 hostname; uname -msr
@@ -15,12 +18,12 @@ clang --version
 echo "
 = User files collection ========================================================
 Collecting user files from Vogsphere
-Repository URL: $1"
+Repository URL: $URL"
 echo "
 = Git history ==================================================================
-$> git -C $2 log --pretty='%H - %an, %ad : %s'"
-git -C $2 log --pretty='%H - %an, %ad : %s'
+$> git -C $GITPATH log --pretty='%H - %an, %ad : %s'"
+git -C $GITPATH log --pretty='%H - %an, %ad : %s'
 echo "
 = Collected files ==========================================
-$> ls -lAR $2"
-ls -lAR $2
+$> ls -lAR $GITPATH"
+ls -lAR $GITPATH
